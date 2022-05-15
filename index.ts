1f37269e025e166071300ec7dbcc287e6fc75d76
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-05-15 15:45:45 +0200

    Added handleError

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import express, { json } from "express";
 import cors from "cors";
 import 'express-async-errors';
+import {handleError} from "./utils/errors";
 
 const app = express();
 
 app.use(cors({
-    origin: 'http:localhost:3000',
+    origin: 'http://localhost:3000',
 }))
 app.use(json());
 
+app.use(handleError);
 app.listen(3001, '0.0.0.0', () => `Server listening on port 3001`)
+
+
