e8742826b02c4895a71fa72716dacbece6e30add
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-06-04 12:33:31 +0200

    Added all endpoints.

--- routers/ad.router.ts
+++ routers/ad.router.ts
@@ -1,9 +1,28 @@
 import { Router } from "express";
+import { AdRecord } from "../records/ad.record";
 
 export const adRouter = Router();
 
-adRouter.get("/", async (req, res) => {
-  res.status(200).json({
-    success: true,
+adRouter
+  .get("/search/:name?", async (req, res) => {
+    const ads = await AdRecord.findAll(req.params.name ?? "");
+    res.status(200).json({
+      success: true,
+      ads,
+    });
+  })
+  .get("/:id", async (req, res) => {
+    const ad = await AdRecord.getOne(req.params.id);
+    res.status(200).json({
+      success: true,
+      ad,
+    });
+  })
+  .post("/", async (req, res) => {
+    const ad = new AdRecord(req.body);
+    await ad.insert();
+    res.status(200).json({
+      success: true,
+      ad,
+    });
   });
-});
